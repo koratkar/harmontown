b19160970575968e86d564ed0170f0fb784cc9ac
Ishaan Koratkar <48111126+koratkar@users.noreply.github.com>
2023-12-03 09:24:17 -0600

    Update add_transcript_to_html.py

--- add_transcript_to_html.py
+++ add_transcript_to_html.py
@@ -1,16 +1,19 @@
 # take transcripts in transcript directory, format them as html, and add them to the html file
 import datetime
+import os
 
 def stamp_to_seconds(stamp):
   return str((datetime.datetime.strptime(stamp, "%H:%M:%S") - datetime.datetime(1900,1,1)).total_seconds())
 
-for file in listdir("transcripts"):
-  input_file = "transcripts/" + file
-  output_file = "website/" + file
-
+for file in os.listdir("transcripts"):
+  filename = file.split(".")[0]
+  input_file = "transcripts/" + file 
+  output_file = "website/" + filename + ".html"
+  
   with open(input_file, 'r') as f_in, open(output_file, 'a+') as f_out:
+
     for line in f_in.read().splitlines():
-       
+      
       time_stamp = line.split(" ")[0][1:] 
       stamp = time_stamp.split(".")[0]
       if 2 > time_stamp.count(":"):
@@ -18,13 +21,11 @@ for file in listdir("transcripts"):
       else: 
         seconds = stamp_to_seconds(stamp)
         
-      text = line.split("  ")[1]
+      text = line.split("] ")[1]
       f_out.seek(0)
       url = f_out.read().split("--")[1]
-
       template_html = '<div class="c"><a class="l" href="#time_stamp" id="time_stamp">link</a> | <div class="s"><a href="url#t=seconds">time_stamp</a></div>\n<div class="t">text</div></div>'
       html = template_html.replace("time_stamp", time_stamp).replace("seconds", seconds).replace("url", url).replace("text", text)
-      
       f_out.write(html)
-    print(input_file + " complete")
 
+  print(file + " complete")
