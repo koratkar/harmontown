354a1109fd16d12733f41f14daa18beed5688a1e
Ishaan Koratkar <48111126+koratkar@users.noreply.github.com>
2023-12-03 07:26:55 -0600

    Create initialize.py

---
+++ initialize.py
@@ -0,0 +1,5 @@
+# unfinished program; once adapted for any podcast, then this will do something.
+# I want it to be some kind of "plug and play" kind of thing as convenient as putting in an RSS feed and coming back the next day 
+# since whisper tiny.en is so small, it opens up a lot of oppurtunities
+# Use whisper.cpp instead?
+#   - file needs to create transcripts, website, vtt_transcripts, episodes dirs
